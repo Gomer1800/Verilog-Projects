6847c8c0ca4f60383eab0832facf2d1696fd082c
Gomer1800 <luisgomez1800@gmail.com>
2018-10-09 21:58:00 -0700

    4bit comparator made

---
+++ lab3_RCA_&_4Bit_Comparator/4BitRCA/4BitRCA.srcs/sources_1/new/RCA.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/04/2018 04:46:56 PM
+// Design Name: 
+// Module Name: RCA
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RCA(
+        input [0:3] A,
+        input [0:3] B,
+        output Cout,
+        output [0:3] S
+    );
+    
+    wire C0,C1,C2;
+    halfadder h1(A[0], B[0], S[0], C0);
+    reducedFullAdder f1(A[1],B[1],C0, S[1], C1);
+    reducedFullAdder f2(A[2],B[2],C1, S[2], C2);
+    reducedFullAdder f3(A[3],B[3],C2, S[3], Cout);
+    
+endmodule
---
+++ lab3_RCA_&_4Bit_Comparator/RCA.v
@@ -0,0 +1,123 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/24/2018 08:37:20 AM
+// Design Name: 
+// Module Name: simTemplate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+/*module simTemplate(
+     );
+    
+      //reg  a, b;
+      reg a,b;
+      wire s,c;
+   
+    //myComponent DUT (
+    reducedFullAdder DUT (
+      //.a(a),
+      .A(a),
+      .B(b),
+      //.b(b),
+      .SUM(s),
+      //.out(out)
+      .Cout(c)
+    );
+   
+    initial begin
+      a = 1'b0;
+      b = 1'b0;
+      c = 1'b0;
+      d = 1'b0;
+      #20
+      a = 1'b0;
+      b = 1'b0;
+      c = 1'b0;
+      d = 1'b1;
+      #20
+      a = 1'b0;
+      b = 1'b0;
+      c = 1'b1;
+      d = 1'b0;
+      #20
+      a = 1'b0;
+      b = 1'b0;
+      c = 1'b1;
+      d = 1'b1;
+      #20
+      a = 1'b0;
+      b = 1'b1;
+      c = 1'b0;
+      d = 1'b0;
+      #20
+      a = 1'b0;
+      b = 1'b1;
+      c = 1'b0;
+      d = 1'b1;
+      #20
+      a = 1'b0;
+      b = 1'b1;
+      c = 1'b1;
+      d = 1'b0;
+      #20
+      a = 1'b0;
+      b = 1'b1;
+      c = 1'b1;
+      d = 1'b1;
+      #20
+      a = 1'b1;
+      b = 1'b0;
+      c = 1'b0;
+      d = 1'b0;
+      #20
+      a = 1'b1;
+      b = 1'b0;
+      c = 1'b0;
+      d = 1'b1;
+      #20
+      a = 1'b1;
+      b = 1'b0;
+      c = 1'b1;
+      d = 1'b0;
+      #20
+      a = 1'b1;
+      b = 1'b0;
+      c = 1'b1;
+      d = 1'b1;
+      #20
+      a = 1'b1;
+      b = 1'b1;
+      c = 1'b0;
+      d = 1'b0;
+      #20
+      a = 1'b1;
+      b = 1'b1;
+      c = 1'b0;
+      d = 1'b1;
+      #20
+      a = 1'b1;
+      b = 1'b1;
+      c = 1'b1;
+      d = 1'b0;
+      #20
+      a = 1'b1;
+      b = 1'b1;
+      c = 1'b1;
+      d = 1'b1;
+      #20                                          
+      $finish;
+    end
+endmodule*/
