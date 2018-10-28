3480aabc0b18a2ecfaa6c10a47e388c1b42f6006
Gomer1800 <luisgomez1800@gmail.com>
2018-10-27 22:59:52 -0700

    restored lab 6

---
+++ lab6/exp11 A Simple Data Latching and Display Circuit/exp11 A Simple Data Latching and Display Circuit.srcs/sources_1/new/exp11.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/25/2018 05:20:53 PM
+// Design Name: 
+// Module Name: exp11
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+/*
+module sseg_dec( input [7:0] ALU_VAL, 
+				 input SIGN,
+				 input VALID,
+                 input CLK,
+                 output [3:0] DISP_EN,
+                 output [7:0] SEGMENTS);
+*/
+                 
+module exp11(
+input CLOCK,
+input [7:0] SWA,
+input btnC,
+
+output [3:0] an,
+output [6:0] seg
+    );
+
+reg [7:0] dFlopInput;
+
+sseg_dec display( SWA, 0, 1, CLOCK, an, seg );
+
+always @ (posedge CLOCK)
+begin
+    if(btnC == 1'b1)
+        dFlopInput =  SWA;
+end
+
+endmodule
